577d93c6904c04189a1b77de1335b2dbfeb7ee84
raunikdev <166943228+raunikdev@users.noreply.github.com>
2026-04-29 01:23:01 +0530

    Time: 550 ms (5.16%) | Memory: 46.8 MB (82.06%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -9,17 +9,39 @@ class Solution {
                 }
             }
         }
-        for(int i=0;i<nums.length;i++){
-            if(i==nums.length-1){
-                return nums[i];
+        if(nums.length==1){
+            return nums[0];
+        }
+        int start = 0;
+        int end = nums.length-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(mid==nums.length-1 || mid == 0){
+                return nums[mid];
             }
-            if(nums[i]!=nums[i+1]){
-                return nums[i];
+            if(mid%2==0){
+                if(nums[mid]==nums[mid+1]){
+                    start = mid +1;
+                }else{
+                    if(nums[mid]==nums[mid-1]){
+                        end=mid-1;
+                    }else{
+                        return nums[mid];
+                    }
+                }
             }else{
-                i++;
+                if(nums[mid]==nums[mid-1]){
+                    start = mid +1;
+                }else{
+                    if(nums[mid]==nums[mid+1]){
+                        end = mid-1;
+                    }else{
+                        return nums[mid];
+                    }
+                }
             }
         }
-        return nums[nums.length-1];
+        return nums[end];
         
     }
 }
