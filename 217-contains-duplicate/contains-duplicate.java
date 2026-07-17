e4a635d55325adf3a850a52413492ab82fe9b5f8
raunikdev <166943228+raunikdev@users.noreply.github.com>
2026-07-17 19:11:41 +0530

    Time: 24 ms (26.05%) | Memory: 92.7 MB (75.57%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            set.add(nums[i]);
+        }
+        if(set.size() == nums.length){
+            return false;
+        }
+        return true;
+    }
+}
